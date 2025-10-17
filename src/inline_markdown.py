dd6a26535ec6fba31ca6e888321edb8e66f57d60
Devin <shout64@gmail.com>
2025-10-17 10:52:53 -0400

    Condense functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,6 @@
 from textnode import *
+import re
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
@@ -13,3 +15,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.append(TextNode(text=sections[2], text_type=TextType.TEXT))
 
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
